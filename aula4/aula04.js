6ab56384cc705747a932da50cb93d699e79412c7
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-24 17:40:09 -0300

    aula 4

---
+++ aula4/aula04.js
@@ -0,0 +1,19 @@
+// Operadores Aritméticos:
+
+
+// let num1 = 10;
+// let num2 = 0;
+// let num3 = num2 = num1 = 10
+
+// console.log(num1, num2, num3);
+
+
+let num1=3, num2=10, res=0;
+
+res = ((num1 + num2) * 6) / 2;
+
+console.log(res)
+res++;
+console.log(res)
+
+console.log(num2 % num1)
