36dee67649953bde596855d001e4e98a9eebbe01
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-26 19:17:17 -0300

    aula 9

---
+++ aula9/aula09.js
@@ -0,0 +1,19 @@
+let n = 10;
+let x = - n
+
+n++ //Incrementando em 1. (Pós Incremento).
+
+++n //Incrementando em 1. (Pós Incremento).
+
+n--; //Decrementando em 1. (Pós Decremento)
+
+--n //Decrementando em 1. (Pré Decremento).
+
+n += 10;
+
+n -= 12;
+
+
+console.log(++n);
+console.log(n);
+console.log(x)
