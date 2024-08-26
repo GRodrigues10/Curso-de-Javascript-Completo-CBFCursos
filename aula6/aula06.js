8e3ea25a30354eefb424bfd542c73fccb1d397e4
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-26 11:38:45 -0300

    atualização

---
+++ aula6/aula06.js
@@ -0,0 +1,11 @@
+// Operadores Relacionais:
+
+let num1=10, num2 = 5, num3 = 10;
+
+console.log(num1 >= num3);
+console.log(num1 <= num2);
+console.log(num1 == num3);
+console.log(!(num1 == num2));
+console.log(num1 != num2);
+
+
