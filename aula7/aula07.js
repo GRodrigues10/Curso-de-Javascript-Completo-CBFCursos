6346b9f7f510b7dcc51203b1422ab7a2c8691742
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-26 12:35:19 -0300

    aula 7

---
+++ aula7/aula07.js
@@ -0,0 +1,22 @@
+// Operadores Lógicos:
+
+let n1=10, n2=5, n3=15, n4=2;
+
+console.log((n1 > n2) && (n3 < n4));
+console.log((n1 > n2) || (n3 < n4));
+console.log(!(n1 > n2) && (n3 < n4));
+console.log(!(n1 > n2) || (n3 < n4));
+
+if(!(n1 > n2) && (n3 > n4)){
+    console.log('Verdadeiro')
+}else{
+    console.log('Falso')
+}
+
+
+if((n1 > n2) || (n3 > n4)){
+    console.log('Verdadeiro')
+}else{
+    console.log('Falso')
+}
+
