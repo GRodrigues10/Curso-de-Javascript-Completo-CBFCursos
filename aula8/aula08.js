c6b52ca5b53275e4e46cf090e662e82bf1166a45
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-26 16:43:36 -0300

    aula 8

--- aula8/aula08.js
+++ aula8/aula08.js
@@ -1,1 +1,24 @@
-  
+// Operadores Bitwise:
+
+// let n1 = 10;
+// let n2 = 11;
+
+// let res = n1 & n2;
+
+// let res2 = n1 | n2;
+
+// let res3 = n1 ^ n2;
+
+
+// console.log(res)
+// console.log(res2)
+// console.log(res3)
+
+
+let num1 = 30;
+
+let resultado = num1 << 1; //Dobra o valor.
+let resultado2 = num1 >> 1; //Dobra o valor.
+console.log(resultado);
+console.log(resultado2); //Pega a metade do valor
+
