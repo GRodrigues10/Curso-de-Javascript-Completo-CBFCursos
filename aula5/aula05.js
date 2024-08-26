dbd7cc3c765770bcfb28bc05df383063eb3b171e
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-26 12:16:34 -0300

    aula 5 e 6

--- aula5/aula05.js
+++ aula5/aula05.js
@@ -1,2 +1,9 @@
+// Operadores Relacionais:
 
+let num1=10, num2 = 5, num3 = 10;
 
+console.log(num1 >= num3);
+console.log(num1 <= num2);
+console.log(num1 == num3);
+console.log(!(num1 == num2));
+console.log(num1 != num2);
--- aula6/aula06.js
+++ aula6/aula06.js
@@ -1,11 +1,38 @@
-// Operadores Relacionais:
 
-let num1=10, num2 = 5, num3 = 10;
+let n1 = 1;
 
-console.log(num1 >= num3);
-console.log(num1 <= num2);
-console.log(num1 == num3);
-console.log(!(num1 == num2));
-console.log(num1 != num2);
+let n2 = '1';
+
+console.log(n1 == n2);
+console.log(n1 === n2);
+
+let nome = prompt('Digite seu nome:');
+
+console.log(nome);
+
+if (navigator.userAgent.match(/Android/i)) {
+    console.log("Dispositivo Android");
+} else if (navigator.userAgent.match(/iPhone/i)) {
+    console.log("Dispositivo iPhone");
+} else if (navigator.userAgent.match(/iPad/i)) {
+    console.log("Dispositivo iPad");
+} else if (navigator.userAgent.match(/Windows/i)) {
+    console.log("Dispositivo Windows");
+} else if (navigator.userAgent.match(/Macintosh/i)) {
+    console.log("Dispositivo macOS");
+} else if (navigator.userAgent.match(/Linux/i)) {
+    console.log("Dispositivo Linux");
+} else {
+    console.log("Dispositivo desconhecido");
+}
+
+
+{
+    let i = 1;
+    console.log(i)
+}
+
+
+
 
 
