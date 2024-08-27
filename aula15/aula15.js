b9b05bf40542df30ada91bf0213f57d0f82e2d8a
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-27 18:18:26 -0300

    aula 15

---
+++ aula15/aula15.js
@@ -0,0 +1,22 @@
+// LOOP - FOR(Definidos).
+
+
+// For:
+console.log('Início do Programa!');
+
+for(let i = 0; i < 10; i++){
+    if(i%2==0){
+        console.log(i + ' = Par')
+    }
+    else{
+        console.log(i + ' = Ímpar')
+    }
+}
+
+
+console.log('Fim do Programa!');
+
+// While:
+
+
+// Do While:
