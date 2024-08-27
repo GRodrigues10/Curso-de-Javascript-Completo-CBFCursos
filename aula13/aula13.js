3a1aa6ae490634d84f6d0ff7e79e7b404ec3ebfd
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-27 13:14:34 -0300

    aula 13

---
+++ aula13/aula13.js
@@ -0,0 +1,23 @@
+let num = 22;
+
+let clima = 'Sol';
+let energia = 65;
+
+if(num < 10){
+    console.log(`${num} é menor que 10`);
+}
+else if(num > 10){
+    console.log(`${num} é maior que 10`);
+}
+else{
+    console.log(`${num} é igual que 10`);
+}
+console.log('Fim do Programa!');
+
+
+if (energia > 70 && clima==='Sol'){
+    console.log('Vou a praia!');
+}
+else{
+    console.log('Vou ao cinema!')
+}
