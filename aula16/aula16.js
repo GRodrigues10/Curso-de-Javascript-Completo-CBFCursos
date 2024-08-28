8decb037745a56726787a37debf9df6136e1db9b
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-28 18:07:12 -0300

    aula 16

---
+++ aula16/aula16.js
@@ -0,0 +1,16 @@
+// LOOP - FOR(Definidos).
+
+
+// For:
+
+let a = [2, 4, 6, 7];
+
+for(num of a){
+    console.log(num);
+}
+
+console.log(...a)
+
+for(let i = 0; i < a.length; i++){
+    console.log(a[i])
+}
