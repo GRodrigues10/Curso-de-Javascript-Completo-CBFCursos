7839181f5fb6a1f4959edbb5557e8289658583ae
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-30 10:33:40 -0300

    aula 16

--- aula16/aula16.js
+++ aula16/aula16.js
@@ -3,14 +3,30 @@
 
 // For:
 
+
+
 let a = [2, 4, 6, 7];
+
 
 for(num of a){
     console.log(num);
 }
 
+for(num in a){
+    console.log(a[num]);
+}
+
+
+
 console.log(...a)
 
 for(let i = 0; i < a.length; i++){
     console.log(a[i])
+}
+
+
+const objs = document.getElementsByTagName('div');
+
+for(ob of objs){
+    console.log(ob)
 }
