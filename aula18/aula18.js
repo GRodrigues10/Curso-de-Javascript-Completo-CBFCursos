c2e95749c777880013b787ffcf70a029c936a3b5
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-30 11:08:16 -0300

    aula 18

---
+++ aula18/aula18.js
@@ -0,0 +1,19 @@
+// Diferença entre While e Do While:
+
+// While = Primeiro faz o teste Lógico. Não tem certeza se vai ser executado.
+
+// Do While = Executa e depois faz o teste Lógico. É executado pelo menos uma vez.
+
+let n = 10;
+
+while(n < 10){
+    console.log('Curso CBF');
+}
+
+
+
+do{
+    console.log('Curso CBF');
+}while(n < 10);
+
+console.log('Fim do Programa!');
