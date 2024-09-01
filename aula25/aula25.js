b45a50da6c202b616c833c19c9eee2003bae56aa
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-01 16:48:05 -0300

    aula 25

---
+++ aula25/aula25.js
@@ -0,0 +1,21 @@
+// Arrow Function:
+
+const soma = (n1, n2) => {
+
+    return n1 + n2
+
+}
+
+console.log(soma(14, 8));
+
+
+
+const nome = n=>{return n};
+
+console.log(nome('Gabriel'))
+
+
+
+const add = n => n + 10;
+
+console.log(add(2))
