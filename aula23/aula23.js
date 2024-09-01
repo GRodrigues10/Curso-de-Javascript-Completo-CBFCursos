f8a4237429d4845776eceab36f7f72c8e8a04f6b
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-01 16:13:17 -0300

    aula 24

---
+++ aula23/aula23.js
@@ -0,0 +1,18 @@
+function soma(...valores){
+    let tam = valores.length
+    let res = 0;
+    // for(let i = 0; i < tam; i++){
+    //     res += valores[i];
+    // }
+
+    // for(n of valores){
+    //     res += n
+    // }
+    
+    for(n in valores){
+        res += valores[n]
+    }
+    return res;
+}
+
+console.log(soma(5, 7, 23, 5));
