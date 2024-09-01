3c7594281f39269b3bcc041e224f34d7a2670ace
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-01 14:10:07 -0300

    aula 22

---
+++ aula22/aula22.js
@@ -0,0 +1,23 @@
+// Retorna a soma:
+function soma(n1=0, n2=0){
+    return n1 + n2;
+}
+
+console.log(soma(4,6));
+
+// Imprime a subtração:
+function sub(n3=0, n4=0){
+    console.log(n3 - n4);
+}
+
+sub(6, 4);
+let valor = 0
+function add(v){
+    return valor + v
+}
+
+valor = add(10)
+console.log(valor)
+
+valor = add(5)
+console.log(valor)
