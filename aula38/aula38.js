819392fc4276df6c61f9d01ae96a89e93e872bd5
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-03 14:53:19 -0300

    aula 38

---
+++ aula38/aula38.js
@@ -0,0 +1,14 @@
+// Parent - Pai
+    // Child - filho 
+    // Sibling - Irmão
+
+
+    const caixa1 = document.querySelector('#caixa1');
+
+    const btn_c = document.querySelectorAll('.curso');
+
+    console.log(caixa1.children[4]);
+    console.log(caixa1.firstElementChild);
+    console.log(caixa1.lastElementChild);
+    console.log(document.getRootNode
+    ));
