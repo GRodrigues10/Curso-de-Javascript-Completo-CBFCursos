6aae45523a9e02f31ea8f4040e83b0e862b42aa5
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-06 14:41:35 -0300

    aula 52

--- aula52/aula52.js
+++ aula52/aula52.js
@@ -1,7 +1,9 @@
 // Iteradores
 
 const valores = [3, 5, 7, 4, 10, 9, 7];
+const textos = ['youtube', 'cachorro', 'jubarte', 'chinês', 'chapolin'];
 const it_valores = valores[Symbol.iterator]();//Criando iterador para o array 'valores'.
+const it_textos = textos[Symbol.iterator]()
 
 console.log(valores);
 console.log(it_valores.next().value);
@@ -13,3 +15,14 @@ console.log(it_valores.next());
 console.log(it_valores.next());
 console.log(it_valores.next());
 console.log(it_valores.next());
+
+console.log('\n\n\n')
+
+console.log(it_textos.next().value);
+console.log(it_textos.next().value);
+console.log(it_textos.next().value);
+console.log(it_textos.next());
+console.log(it_textos.next());
+console.log(it_textos.next());
+console.log(it_textos.next());
+
