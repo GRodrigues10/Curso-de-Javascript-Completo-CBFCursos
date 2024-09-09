27709b6ca412900c286e1774dfad1ddea08baf52
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-09 14:26:04 -0300

    aula 57

---
+++ aula57/aula57.js
@@ -0,0 +1,21 @@
+// Objetos
+
+class Pessoa{
+    constructor(idade){
+        this.nome = 'Gabriel';
+        this.idade = idade;
+    }
+}
+
+let p1 = new Pessoa(22);
+let p2 = new Pessoa(12);
+let p3 = new Pessoa(42);
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+
+
+
+
+
