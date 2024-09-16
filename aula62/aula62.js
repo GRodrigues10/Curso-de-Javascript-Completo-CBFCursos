30ff21221917f97034c9903f1a5cf685bac13252
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-16 11:14:35 -0300

    aula 62

--- aula62/aula62.js
+++ aula62/aula62.js
@@ -23,15 +23,44 @@ class Carro{ //Classe PAI
 
 class Militar extends Carro{
     constructor(nome, portas, blindagem, municao){
-        super(nome, po        rtas);
+        super(nome, portas);
+        this.blindagem = blindagem;
+        this.municao = municao
+        this.setCor('Verde');
+    }
+    atirar = ()=>{
+        if(this.municao > 0){
+            this.municao --
+        }
+    }
+}
 
-
+class Utilitario extends Carro{
+    constructor(nome, portas, arcondicionado){
+        super(nome, portas);
+        this.arcondicionado = arcondicionado;
     }
 }
 
 const c1 = new Carro('Normal', 5, 'Azul');
 c1.setCor('Azul')
+
+
+
+const c2 = new Militar('Tanque', 1, 100, 50);
+c2.atirar();
+c2.atirar();
+c2.atirar();
+c2.atirar();
+
 console.log(`Nome:${c1.nome}`);
 console.log(`Portas:${c1.portas}`);
 console.log(`Ligado:${c1.ligado}`);
 console.log(`Cor:${c1.cor}`);
+console.log('--------------')
+
+console.log(`Nome:${c2.nome}`);
+console.log(`Portas:${c2.portas}`);
+console.log(`Munição:${c2.municao}`);
+console.log(`Blindagem:${c2.blindagem}`);
+console.log(`Cor:${c2.cor}`);
