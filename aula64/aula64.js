b2baa7f7a4e1e19c75bc4bd8fd41b48efb7f7f1a
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-17 17:45:42 -0300

    aula 64

--- aula64/aula64.js
+++ aula64/aula64.js
@@ -17,8 +17,8 @@ const pessoa = {
 
 const string_pessoa = '{"nome":"Gabriel","canal":"CFB Cursos","curso":"Javascript","aulas":{"aula01":"Introdução","aula02":"Variáveis","aula03":"Condicional"}'
 
-const s_json_pesssoa = JSON.stringify(pessoa);
-const o_json_pesssoa = JSON.stringify(pessoa);
+const s_json_pesssoa = JSON.stringify(pessoa);//Converte objeto em string json
+const o_json_pesssoa = JSON.stringify(pessoa);// Converte string json em objeto
 console.log(pessoa)
 console.log(s_json_pesssoa)
 console.log(o_json_pesssoa)
