d304cf5225601c248f91f0f3f7eb1cbc89e6a41b
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-17 17:44:07 -0300

    aula 64

--- aula64/aula64.js
+++ aula64/aula64.js
@@ -1,1 +1,24 @@
 // Objetos 
+
+const pessoa = {
+    nome:'Gabriel',
+    canal:'CFB Cursos',
+    curso: 'Javascript',
+    aulas:{
+        aula01:'Introdução',
+        aula02:'Variáveis',
+        aula03:'Condicional'
+    }
+}
+
+// console.log(pessoa);
+// console.log(pessoa.nome);
+// console.log(pessoa.aulas)
+
+const string_pessoa = '{"nome":"Gabriel","canal":"CFB Cursos","curso":"Javascript","aulas":{"aula01":"Introdução","aula02":"Variáveis","aula03":"Condicional"}'
+
+const s_json_pesssoa = JSON.stringify(pessoa);
+const o_json_pesssoa = JSON.stringify(pessoa);
+console.log(pessoa)
+console.log(s_json_pesssoa)
+console.log(o_json_pesssoa)
