a035fd9b83d784df18880dbf5c3cf6d1f5066d3a
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-20 14:19:48 -0300

    aula 67

---
+++ aula67/aula67.js
@@ -0,0 +1,28 @@
+// PROTOTYPES - Quando precisar adicionar uma propriedade ou método para um objeto.
+
+const Nave = function(energia){
+    this.energia = energia;
+    this.disparos = 20;
+}
+
+
+const n1 = new Nave(100);
+
+Nave.prototype.vidas = 3;//Adicionando vidas no objeto.
+Nave.prototype.disparar = function(){
+    if(this.disparos > 0){
+        this.disparos--
+    }
+}
+
+
+console.log(Nave)
+console.log(n1)
+console.log(n1.energia)
+console.log(n1.vidas)
+console.log(n1.disparos)
+n1.disparar();
+n1.disparar();
+n1.disparar();
+
+console.log(n1.disparos)
