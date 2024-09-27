7ac543da674f8871273272a50650ffb56f2d1694
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-27 17:02:30 -0300

    Update calculadora.js

--- aula70-71-72-73/calculadora.js
+++ aula70-71-72-73/calculadora.js
@@ -2,6 +2,8 @@ const  teclasNum = [...document.querySelectorAll('num')];
 const  teclasOp = [...document.querySelectorAll('op')];
 const  teclaRes = document.querySelector('.res');
 const  display = document.querySelector('.display');
+const teclaOnOff = document.getElementById('teclaOnOff');
+const teclaLimpar = document.getElementById('teclaLimpar')
 
 teclasNum.map((el)=>{
     el.addEventListener('click', (e) =>{
@@ -13,4 +15,8 @@ teclasOp.map((el)=>{
     el.addEventListener('click', (e) =>{
         display.innerHTML += e.target.innerHTML
     })
+})
+
+teclaLimpar.addEventListener('click', (e)=>{
+    display.innerHTML = 0
 })
