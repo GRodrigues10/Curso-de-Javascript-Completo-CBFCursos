7a5e6c9c6b4102b92b374c689ed7f011d2ae7e8d
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-10-16 14:34:12 -0300

    aula 77

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -1,2 +1,10 @@
 const data = new Date();
-console.log(data);
+// console.log(Date.now());//Timestamp
+
+console.log(data.getDate());// Dia do mês
+console.log(data.getDay());// Dia da semana
+console.log(data.getFullYear());// Ano com 4 dígitos
+console.log(data.getHours());// Horas
+console.log(data.getMilliseconds());// Milisegundos
+console.log(data.getMinutes());// Minutos
+console.log(data.getSeconds());// Segundos ...
