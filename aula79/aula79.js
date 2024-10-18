512062f56efb6507d9cc05f57870e5a267257064
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-10-18 15:00:14 -0300

    aula 79

--- aula79/aula79.js
+++ aula79/aula79.js
@@ -1,1 +1,26 @@
 // Timer
+
+// Relógio:
+const div_relogio = document.getElementById('div-relógio');
+
+const data = new Date();
+
+const relógio = () => {
+const data = new Date();
+let horas = data.getHours();
+horas = horas < 10 ? '0' + horas : horas;
+
+let minutos = data.getMinutes();
+minutos = minutos < 10 ? '0' + minutos : minutos;
+
+let segundos = data.getSeconds();
+segundos = segundos < 10 ? '0' + segundos : segundos
+
+let hora_formatada = horas + ':' + minutos + ':' + segundos
+div_relogio.innerHTML = hora_formatada;
+}
+
+
+const intervalo = setInterval(relógio, 1000);
+
+
