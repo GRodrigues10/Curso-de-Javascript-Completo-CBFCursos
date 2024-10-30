b615f6cce374c6eaa55b7b7dcd7ec0cf1967e6cb
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-10-30 17:57:25 -0300

    aula 86

--- aula82-83-84-85-86/aula82-83-84-85-86.js
+++ aula82-83-84-85-86/aula82-83-84-85-86.js
@@ -58,8 +58,32 @@ class Bola{
         this.palco.appendChild(div);
     }
 
-    controlar = () =>{
+    controle_bordas = () =>{
+        if(this.px + this.tam >= larguraPalco){
+            this.dirx = - 1;
+        }
 
+        else if(this.px <= 0){
+            this.dirx = 1
+        }
+
+        if(this.py + this.tam >= alturaPalco){
+            this.diry = - 1;
+        }
+
+        else if(this.py <= 0){
+            this.diry = 1
+        }
+    }
+
+    controlar = () =>{
+        this.controle_bordas();
+        this.px += this.dirx * this.velx;
+        this.py += this.diry * this.vely;
+        this.eu.setAttribute('style', `left:${this.px}; top:${this.py}; width:${this.tam}; height: ${this.tam}; background-color = rgb(${this.r}, ${this.g}, ${this.b}) `);
+        if((this.px > larguraPalco) || (this.py > alturaPalco)){
+            this.remover();
+        }
     }
 }
 
