e4c448988424bf71149a7397819e4c851405a091
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-11-06 13:00:58 -0300

    aula 87

--- aula87/aula87.js
+++ aula87/aula87.js
@@ -1,16 +1,15 @@
-const vader = [...document.getElementsByClassName('vader')];
+const url = document.getElementById('url');
+const btn_url = document.getElementById('btn_url');
 
-let x = 0
-let y = 0
-
-
-window.addEventListener('mousemove', (e)=>{
-    x = e.clientX;
-    y = e.clientY;
-
-    const rotação = Math.atan2(x, y) * 180/Math.PI;// Retorna o arco tangente de dois coeficientes(x e y) - retorna radiano, tem que converter para graus:* 180/Math.PI;
+btn_url.addEventListener('click', (evt)=>{
+    // window.location = 'https://www.google.com.br';
+    // window.location.replace('https://www.google.com.br');//Em algumas ocasions ele vai exlcuir a url do histórico e substituir pela a que vc deseja
+    // window.location.assign('https://www.google.com.br');// Faz a mesma coisa mais não remove a url corrente.
+    // window.location.reload('https://www.google.com.br');// Recarrega a página.
+    // window.history.back();//Volta no histórico, ou seja... algo anterior no histórico.
+    // window.history.forward();//Avança no histórico, ou seja... algo posterior no histórico.
+    // window.history.go(1);// Vai para a página pra frente dependendo do número dentro do parêntesis.
+    // console.log(window.history.length);
+    window.location = url.value
     
-    vader.forEach((o)=>{
-        o.style.transform = "rotate("+ rotação +" deg)";
-    });
-});
+})
