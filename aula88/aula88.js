b79e1afdb1cf23174cfdcbcf3ae48a331af7d198
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-11-07 14:45:45 -0300

    aula 88

---
+++ aula88/aula88.js
@@ -0,0 +1,20 @@
+const btn_alert = document.getElementById('btn_alert');
+const btn_confirm = document.getElementById('btn_confirm');
+const btn_prompt = document.getElementById('btn_prompt');
+
+btn_alert.addEventListener('click', (e)=>{
+   alert('Olá, como você está?');
+});
+
+btn_confirm.addEventListener('click', (e)=>{
+    const retorno = confirm('Você está aprendendo muito?');
+    if(retorno){
+        console.log('Botão "ok" pressionado!');
+    }else{
+        console.log('Botão "cancelar" pressinado!')
+    }
+});
+
+btn_prompt.addEventListener('click', (e)=>{
+    prompt('Digite algo e no prompt');
+});
