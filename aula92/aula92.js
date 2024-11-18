ad488ac5ae13af818cfe9359d7a4fba7926b751d
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-11-18 15:01:28 -0300

    Update aula92.js

---
+++ aula92/aula92.js
@@ -0,0 +1,8 @@
+import { cursos } from "../cursos.js";
+import { carros } from "../cursos.js";
+
+// const obj = document.getElementById('objetos');
+
+console.log('Servidor Rodando');
+console.log(cursos);
+console.log(carros);
