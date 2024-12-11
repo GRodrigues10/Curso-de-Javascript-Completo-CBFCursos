5cbef93a65df4d22c99f17f749fcda72d376b74c
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-12-11 16:47:24 -0300

    atualização aula 98

--- aula98/aula98.js
+++ aula98/aula98.js
@@ -9,3 +9,7 @@ console.log(s2);
 console.log(s1==s2);
 console.log(s1===s2);//False
 console.log(s3===s4);//True
+
+console.log(Symbol.keyFor(s1));//Undefined, pois trabalha apenas com o registrador global
+console.log(Symbol.keyFor(s3));//Gabriel
+console.log(Symbol.keyFor(s4));//Gabriel
