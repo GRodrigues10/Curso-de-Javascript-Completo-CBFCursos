be74358afd49f29cab998ca5b78ac67b2b176be2
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-12-11 16:45:12 -0300

    atualização aula 98

--- aula98/aula98.js
+++ aula98/aula98.js
@@ -1,7 +1,11 @@
 const s1 = Symbol();//Valor único, não pode ser usado no DOM.
 const s2 = Symbol();
 
+const s3 = Symbol.for('Gabriel');
+const s4 = Symbol.for('Gabriel');
+
 console.log(s1);
 console.log(s2);
 console.log(s1==s2);
-console.log(s1===s2);
+console.log(s1===s2);//False
+console.log(s3===s4);//True
