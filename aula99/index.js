a3a5a86f7edeb81eac19d99868d945a315f69c57
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-12-20 10:16:35 -0300

    aula 99

--- aula99/index.js
+++ aula99/index.js
@@ -1,7 +1,14 @@
 class Jogador{
     constructor(nome){
-        this.nome = nome
-        this.id = Symbol()
+        this.nome = nome;
+        this.id = Symbol();
+    }
+}
+
+class Tecnico{
+    constructor(nome){
+        this.nome = nome;
+        this.id = Symbol();
     }
 }
 
@@ -9,5 +16,14 @@ let jogadores = [new Jogador('j1'),new Jogador('j2'),new Jogador('j3'),new Jogad
 let s1 = jogadores[0].id;
 jogadores= jogadores.filter((j)=>{
     return j.id != s1;
-})
+});
 console.log(jogadores);
+
+
+let tecnicos = [new Tecnico('T1'),new Tecnico('T2'),new Tecnico('T3'),new Tecnico('T4')];
+let t1 = tecnicos[0].id;
+
+tecnicos = tecnicos.filter((t)=>{
+    return t.id!=t1
+});
+console.log(tecnicos);
